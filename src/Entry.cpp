1f28d4556a3fc39861ce212509534f6096f9157c
Tsubasa6848 <komatsu_moho@outlook.com>
2024-03-25 20:39:28 +0800

    adapt: adapt GMLIB 0.9.8

--- src/Entry.cpp
+++ src/Entry.cpp
@@ -5,14 +5,17 @@ ll::Logger logger(PLUGIN_NAME);
 
 namespace GMWhitelist {
 
-namespace {
+std::unique_ptr<Entry>& Entry::getInstance() {
+    static std::unique_ptr<Entry> instance;
+    return instance;
+}
 
-std::unique_ptr<std::reference_wrapper<ll::plugin::NativePlugin>>
-    selfPluginInstance; // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
+bool Entry::load() {
+    initPlugin();
+    return true;
+}
 
-auto disable(ll::plugin::NativePlugin& /*self*/) -> bool { return true; }
-
-auto enable(ll::plugin::NativePlugin& /*self*/) -> bool {
+bool Entry::enable() {
     RegisterCommands();
     listenEvent();
     logger.info("GMWhitelist Loaded!");
@@ -21,34 +24,8 @@ auto enable(ll::plugin::NativePlugin& /*self*/) -> bool {
     return true;
 }
 
-auto load(ll::plugin::NativePlugin& self) -> bool {
-    selfPluginInstance = std::make_unique<std::reference_wrapper<ll::plugin::NativePlugin>>(self);
-    initPlugin();
-    return true;
-}
-
-auto unload(ll::plugin::NativePlugin& self) -> bool {
-    selfPluginInstance.reset();
-    return true;
-}
-
-} // namespace
-
-auto getSelfPluginInstance() -> ll::plugin::NativePlugin& {
-    if (!selfPluginInstance) {
-        throw std::runtime_error("selfPluginInstance is null");
-    }
-
-    return *selfPluginInstance;
-}
+bool Entry::disable() { return true; }
 
 } // namespace GMWhitelist
 
-extern "C" {
-_declspec(dllexport) auto ll_plugin_disable(ll::plugin::NativePlugin& self) -> bool {
-    return GMWhitelist::disable(self);
-}
-_declspec(dllexport) auto ll_plugin_enable(ll::plugin::NativePlugin& self) -> bool { return GMWhitelist::enable(self); }
-_declspec(dllexport) auto ll_plugin_load(ll::plugin::NativePlugin& self) -> bool { return GMWhitelist::load(self); }
-_declspec(dllexport) auto ll_plugin_unload(ll::plugin::NativePlugin& self) -> bool { return GMWhitelist::unload(self); }
-}
+LL_REGISTER_PLUGIN(GMWhitelist::Entry, GMWhitelist::Entry::getInstance());
